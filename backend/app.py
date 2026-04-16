f6e93a028ff4736c2120d98e0dbcf4574c746a80
FathimaNafra <fathumanafra5@gmail.com>
2026-04-16 11:23:06 +0530

    Validate indicators/Create rule-based logic/ Test on assets

--- backend/app.py
+++ backend/app.py
@@ -1,20 +1,59 @@
 from data_fetcher import get_market_data
+from indicators import calculate_rsi, calculate_ema, calculate_macd
+from utils import validate_indicators
+from strategy import generate_signal
 
-# Test Crypto
-df = get_market_data(
-    asset_type="crypto",
-    symbol="BTCUSDT",
+def process_asset(asset_type,symbol,timeframe):
+    print(f"\nFetching data for {symbol} ({asset_type})...")
+
+    df=get_market_data(
+        asset_type=asset_type,
+        symbol=symbol,
+        timeframe=timeframe
+    
+    )
+
+    print("Latest Market Data:")
+    print(df.tail())
+
+    print("\nCalculating indicators...")
+
+    df = calculate_rsi(df)
+    df = calculate_ema(df)
+    df = calculate_macd(df)
+
+    validate_indicators(df)
+
+    print("\nGenerating Trading Signal...")
+    signal=generate_signal(df)
+
+    print("Signal Result:")
+    print(signal)
+
+    return signal
+
+if __name__=="__main__":
+
     timeframe="1h"
-)
+   
+    # Test Crypto
+    crypto_signal = process_asset(
+        asset_type="crypto",
+        symbol="BTCUSDT",
+        timeframe=timeframe
+    )
 
-print(df.tail())
+    # Test Forex
+    forex_signal = process_asset(
+        asset_type="forex",
+        symbol="EUR/USD",
+        timeframe=timeframe
+    )
+
+    print("\nFinal Results")
+    print("Crypto Signal:", crypto_signal)
+    print("Forex Signal:", forex_signal)
+   
 
 
-#Test Forex
-df = get_market_data(
-    asset_type="forex",
-    symbol="EUR/USD",
-    timeframe="1h"
-)
 
-print(df.tail())
